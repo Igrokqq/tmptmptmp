59ad63178caf54ccbaccdc836f16b655aa1e2bbf
Vladyslav Surin <surinvladyslav@gmail.com>
2023-11-17 16:22:26 +0200

    fix. router

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import './index.css';
-import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <HashRouter>
-          <App />
-      </HashRouter>
+      <App />
   </React.StrictMode>
 );
